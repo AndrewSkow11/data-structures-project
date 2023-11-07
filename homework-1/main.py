dcac82082f7233b4672e5036df4fd79c733b55b2
Andrew Skow <skovorodnikov11@gmail.com>
2023-11-07 23:55:03 +0300

    подготовлен релиз без тестов

--- homework-1/main.py
+++ homework-1/main.py
@@ -8,12 +8,17 @@ if __name__ == '__main__':
     print(n1)  # <__main__.Node object at 0x0000022803036050>
     print(n2.next_node)  # <__main__.Node object at 0x0000022803036050>
 
+    # пушинг
     stack = Stack()
     stack.push('data1')
     stack.push('data2')
     stack.push('data3')
+
+    # выгрузка начиная с верхушки
     print(stack.top.data)  # data3
     print(stack.top.next_node.data)  # data2
     print(stack.top.next_node.next_node.data)  # data1
     print(stack.top.next_node.next_node.next_node)  # None
-    print(stack.top.next_node.next_node.next_node.data)  # AttributeError: 'NoneType' object has no attribute 'data'
+
+    # чтобы код завершался без ошибки нижняя строка закомментирована
+    # print(stack.top.next_node.next_node.next_node.data)  # AttributeError: 'NoneType' object has no attribute 'data'
--- src/stack.py
+++ src/stack.py
@@ -1,34 +1,37 @@
 class Node:
     """Класс для узла стека"""
-
-    def __init__(self, data, next_node):
+    def __init__(self, data=None, next_node=None):
         """
         Конструктор класса Node
-
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
     """Класс для стека"""
-
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        # инициализирован пустой список
+        self.top = None
+
 
     def push(self, data):
         """
         Метод для добавления элемента на вершину стека
-
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        new_node = Node(data)
+        new_node.next_node = self.top
+        self.top = new_node
 
     def pop(self):
         """
         Метод для удаления элемента с вершины стека и его возвращения
-
         :return: данные удаленного элемента
         """
-        pass
+        item = self.top.data
+        self.top = self.top.next_node
+        return item
+
